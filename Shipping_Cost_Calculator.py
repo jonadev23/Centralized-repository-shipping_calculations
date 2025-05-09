7ed3a3e6aa04c5be8e6881db268ed4af4e16a830
jonadev23 <jonathandevops34@gmail.com>
2025-05-09 02:57:35 -0400

    Revert "Added additional line to the repo"
    This reverts commit 065dab7ba08a3618b05f442b2de25f9dfd1e6926.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,7 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by jonadev23
-
- # Here is another update by <your GitHub username>
+ # Here is a new update by jona
 
